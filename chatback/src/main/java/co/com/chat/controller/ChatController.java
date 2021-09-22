70d23b64d7d94225f6cd623dc5fb0abebcfa2646
Cristian Alejandro Wilches Ferreira <cawilchesf@sbrcentroo.local>
2021-09-22 16:24:18 -0500

    Chat backend semfuncional

--- chatback/src/main/java/co/com/chat/controller/ChatController.java
+++ chatback/src/main/java/co/com/chat/controller/ChatController.java
@@ -1,22 +1,50 @@
 package co.com.chat.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import co.com.chat.model.Mensaje;
 
 @Controller
 public class ChatController {
-	
-	@MessageMapping("/chatGeneral")
-	@SendTo("/app-chat/mensaje")
-	public Mensaje recibeMensaje(Mensaje mensaje) {
-		mensaje.setTimestamp(new Date().getTime());
-		mensaje.setMensaje("El mensaje recibido es: ".concat(mensaje.getMensaje()));
-		
-		return mensaje;
+
+	private String[] colores = { "red", "green", "blue", "magenta", "purple", "orange" };
+	private List<String> canales = new ArrayList<>();
+	@Autowired
+	private SimpMessagingTemplate messaginTemplate;
+
+	@MessageMapping("/chatGeneral/{to}")
+	// @SendTo("/app-chat/mensaje")
+	public void recibeMensaje(@DestinationVariable String to, Mensaje mensaje) {
+		try {
+			System.err.println(to);
+			/*
+			 * if (canales.size() == 0) { canales.add("General"); }
+			 */
+			mensaje.setTimestamp(new Date().getTime());
+			if (mensaje.getTipo().equals("NEW USER")) {
+				mensaje.setMensaje("nuevo usuario ".concat(mensaje.getMensaje()));
+				mensaje.setColor(colores[new Random().nextInt(colores.length)]);
+				if (canales.indexOf(mensaje.getUsername()) == -1) {
+					canales.add(mensaje.getUsername());
+				}
+				mensaje.setListUser(canales);
+			}
+			System.err.println(mensaje.toString());
+			messaginTemplate.convertAndSend("/app-chat/mensaje", mensaje);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			// System.err.println(e.getCause().getMessage());
+			mensaje.setMensaje("Mensaje No Recibido");
+		}
 	}
 }
--- chatback/src/main/java/co/com/chat/model/Mensaje.java
+++ chatback/src/main/java/co/com/chat/model/Mensaje.java
@@ -1,21 +1,32 @@
 package co.com.chat.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Mensaje implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	private String Mensaje;
-	
+
+	private String mensaje;
+
 	private long timestamp;
 
+	private String dentination;
+
+	private String username;
+
+	private String tipo;
+
+	private String color;
+
+	private List<String> listUser;
+
 	public String getMensaje() {
-		return Mensaje;
+		return this.mensaje;
 	}
 
 	public void setMensaje(String mensaje) {
-		Mensaje = mensaje;
+		this.mensaje = mensaje;
 	}
 
 	public long getTimestamp() {
@@ -25,6 +36,52 @@ public class Mensaje implements Serializable {
 	public void setTimestamp(long timestamp) {
 		this.timestamp = timestamp;
 	}
-	
-	
+
+	public String getDentination() {
+		return dentination;
+	}
+
+	public void setDentination(String dentination) {
+		this.dentination = dentination;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public List<String> getListUser() {
+		return listUser;
+	}
+
+	public void setListUser(List<String> listUser) {
+		this.listUser = listUser;
+	}
+
+	@Override
+	public String toString() {
+		return "Mensaje [color=" + color + ", dentination=" + dentination + ", listUser=" + listUser.toString()
+				+ ", mensaje=" + mensaje + ", timestamp=" + timestamp + ", tipo=" + tipo + ", username=" + username
+				+ "]";
+	}
+
 }
